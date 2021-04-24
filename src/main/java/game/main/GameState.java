5beaa497aa164e7037aa4a5fd4f6bf1da9f4343d
Michael Connor <michaelconnor468@gmail.com>
2021-04-24 13:35:40 -0400

    added logic to check if game state can transition from state to state

--- src/main/java/game/main/GameState.java
+++ src/main/java/game/main/GameState.java
@@ -1,12 +1,39 @@
 package game.main;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class GameState {
     public enum State {
         MainMenu,
+        Loading,
         Playing,
         Paused
     }
-    public State state;
+    private State state;
+    private HashMap<State, HashSet<State>> canTransitionTo;
 
-    public GameState() { state = state.MainMenu; }
+    public GameState() { 
+        state = state.MainMenu;
+        canTransitionTo = new HashMap<State, HashSet<State>>();
+        for ( State s : State.values() )
+            canTransitionTo.put(s, new HashSet<State>());
+
+        canTransitionTo.get(state.MainMenu).add(state.Loading);
+
+        canTransitionTo.get(state.Loading).add(state.Playing);
+
+        canTransitionTo.get(state.Playing).add(state.Paused);
+
+        canTransitionTo.get(state.Paused).add(state.MainMenu);
+    }
+    public State getState() { return state; }
+    public boolean setState(State state) {
+        if ( canTransitionTo.get(this.state).contains(state) ) {
+            this.state = state;
+            return true;
+        }
+        else
+            return false;
+    }
 }
