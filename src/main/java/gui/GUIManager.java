672a224c3218414cdafc2057841839ec4df1810b
Michael Connor <michaelconnor468@gmail.com>
2021-04-23 22:09:34 -0400

    deleted GUIManager class

--- src/main/java/gui/GUIManager.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-/**
- * Made to act as a "docking" interface between guis that will be in use allowing only the currently in use one to dock. This allows for easy swapping
- * between guis such as start menu, gmae, inventory, e.x.
- */
-public class GUIManager {
-
-}
