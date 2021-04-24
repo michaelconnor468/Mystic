1147ed35205ef06b28b2bc81f7316264b7b92294
Michael Connor <michaelconnor468@gmail.com>
2021-04-24 11:25:12 -0400

    made WindowManager open up a window when created

--- src/main/java/game/main/WindowManager.java
+++ src/main/java/game/main/WindowManager.java
@@ -1,7 +1,23 @@
 package game.main;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class WindowManager {
     public WindowManager() {
-    
+        Application.launch(App.class); 
+    }
+
+    public static class App extends Application {
+        @Override
+        public void start(Stage stage) {
+            StackPane mainPane = new StackPane();
+            Scene scene = new Scene(mainPane, 500, 300);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
