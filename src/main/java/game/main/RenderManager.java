9250a2a57d11ce63192e7bd603258015ffea4414
Michael Connor <michaelconnor468@gmail.com>
2021-05-06 20:37:05 -0400

    wrote base logic for RenderManager

--- src/main/java/game/main/RenderManager.java
+++ src/main/java/game/main/RenderManager.java
@@ -1,9 +1,44 @@
 package game.main;
 
+import util.parse.obj.*;
+import game.main.render.*;
+
+import java.util.ArrayList;
+
 /**
  * Manages rendering within the game through an object interface Renderer that is passed to objects that must be rendered. Decouples rendering implementations
  * from actual interfaces that objects use. Synchronises the rendering of objects allowing rendering to differ from in game ticks for different refresh rates and performance.
  */
 public class RenderManager {
-    
+    private X x;
+    private int ticksPerRender, ticksElapsed;
+    private ArrayList<Renderable> toRender;
+    private Renderer renderer;
+
+    private RenderManager() {}
+    public RenderManager(X x, ParserBlock settings) {
+        this.x = x;
+        this.ticksPerRender = 2;
+        this.ticksElapsed = 0;
+        this.toRender = new ArrayList<>();
+    }
+
+    public void tick(X x) {
+        if ( ticksPerRender == ticksElapsed++ )
+            render();
+    }
+
+    public void register(Renderable obj) {
+        if (!toRender.contains(obj))
+            toRender.add(obj);
+    }
+
+    public void unregister(Renderable obj) {
+        toRender.remove(obj);
+    }
+
+    private void render() {
+        for ( Renderable r : toRender )
+            r.render(renderer);
+    }
 }
