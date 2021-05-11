df286b50afe355630fd7206293722d08312d9dbb
Michael Connor <michaelconnor468@gmail.com>
2021-05-11 18:55:14 -0400

    fixed render manager printing out images not on canvas

--- src/main/java/game/main/render/Renderer.java
+++ src/main/java/game/main/render/Renderer.java
@@ -13,20 +13,25 @@ import javafx.scene.image.Image;
 public class Renderer {
     private X x;
     private GraphicsContext gc; 
+    private int resolutionx, resolutiony;
 
     private Renderer() {}
     public Renderer(X x, GraphicsContext gc) {
         this.x = x;
         this.gc = gc;
+        this.resolutionx = ((ParserInt) x.getMainSettings().getProperties().get("resolutionx")).getNumber(); 
+        this.resolutiony = ((ParserInt) x.getMainSettings().getProperties().get("resolutiony")).getNumber(); 
     }
 
     public void render(Animation animation) {
-        int xCorner = x.getPlayer().getxPosition() + x.getPlayer().getxSize()/2 -  
-            (((ParserInt) x.getMainSettings().getProperties().get("resolutionx")).getNumber()/2);
-        int yCorner = x.getPlayer().getyPosition() + x.getPlayer().getySize()/2 - 
-            (((ParserInt) x.getMainSettings().getProperties().get("resolutiony")).getNumber()/2);
+        int xCorner = x.getPlayer().getxPosition() + x.getPlayer().getxSize()/2 - (resolutionx/2);
         int xRenderLocation = animation.getEntity().getxPosition() - xCorner; 
+        if ( xRenderLocation > resolutionx || xRenderLocation + animation.getEntity().getxSize() < 0 )
+            return;
+        int yCorner = x.getPlayer().getyPosition() + x.getPlayer().getySize()/2 - (resolutiony/2);
         int yRenderLocation = animation.getEntity().getyPosition() - yCorner; 
+        if ( yRenderLocation > resolutiony || yRenderLocation + animation.getEntity().getySize() < 0 )
+            return;
         gc.drawImage(animation.getImage(), xRenderLocation, yRenderLocation);
     }
 }
