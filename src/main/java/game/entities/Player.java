a7e267403aad98cdb6f0fb5bbf79ae56db0a69c5
Michael Connor <michaelconnor468@gmail.com>
2021-05-12 08:41:23 -0400

    added player speed property

--- src/main/java/game/entities/Player.java
+++ src/main/java/game/entities/Player.java
@@ -76,6 +76,7 @@ public class Player extends DynamicEntity implements DestructibleEntity {
         player.walkLeftAnimation = new Animation(x, player, Paths.get("src/main/resources/player/walk_left.png"));
         player.walkRightAnimation = new Animation(x, player, Paths.get("src/main/resources/player/walk_right.png"));
         player.currentAnimation = player.walkUpAnimation;
+        player.speed = ((ParserInt) map.get("speed")).getNumber();
         x.getTimingManager().register(player);
         return player;
     }
