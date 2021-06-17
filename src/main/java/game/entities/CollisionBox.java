11ad14056e246f89df6f6c8a1544b861f1710368
Michael Connor <michaelconnor468@gmail.com>
2021-06-17 19:47:00 -0400

    implemented collision box standalone class

--- src/main/java/game/entities/CollisionBox.java
+++ src/main/java/game/entities/CollisionBox.java
@@ -1,5 +1,57 @@
 package game.entities;
 
-public class CollisionBox {
+import util.parse.obj.*;
 
+import java.util.Map;
+import java.awt.Point;
+
+public class CollisionBox {
+    private Point min, max;
+    private Entity entity;
+
+    private CollisionBox() {}
+
+    public CollisionBox(Entity entity, ParserBlock block) {
+        Map<String, ParserObject> props = block.getProperties();
+        this.min = new Point(((ParserInt) props.get("xMin")).getNumber(), ((ParserInt) props.get("xMax")).getNumber());
+        this.max = new Point(((ParserInt) props.get("yMin")).getNumber(), ((ParserInt) props.get("yMax")).getNumber());
+        this.entity = entity;
+    }
+
+    public CollisionBox(Entity entity, Point min, Point max) {
+        this.min = new Point(min);
+        this.max = new Point(max);
+        this.entity = entity;
+    }
+
+    public boolean collidesWith(CollisionBox collisionBox) {
+        return !(
+            this.getRealMax().getX() < collisionBox.getRealMin().getX() ||
+            this.getRealMin().getX() > collisionBox.getRealMax().getX() ||
+            this.getRealMax().getY() < collisionBox.getRealMin().getY() ||
+            this.getRealMin().getY() > collisionBox.getRealMax().getY() 
+        );
+    }
+
+    public Point getRealMin() { 
+        Point ret = new Point(entity.getPosition()); 
+        ret.translate((int) min.getX(), (int) min.getY()); 
+        return ret;
+    }
+
+    public Point getRealMax() { 
+        Point ret = new Point(entity.getPosition()); 
+        ret.translate((int) max.getX(), (int) max.getY()); 
+        return ret;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        sb.append("xMin: " + min.getX());
+        sb.append(",xMax: " + max.getX());
+        sb.append(",yMin: " + min.getY());
+        sb.append(",yMax: " + max.getY());
+        sb.append("}");
+        return sb.toString();
+    };
 }
