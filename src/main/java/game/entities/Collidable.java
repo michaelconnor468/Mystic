34a669d4cd5cd0e8391fb415e08bd4234ddb0daf
Michael Connor <michaelconnor468@gmail.com>
2021-06-18 19:46:38 -0400

    documented collision interface, implemented passability, and changed inner Entity to more specific Collidable

--- src/main/java/game/entities/Collidable.java
+++ src/main/java/game/entities/Collidable.java
@@ -3,8 +3,14 @@ package game.entities;
 import java.awt.Point;
 import java.util.Collection;
 
+/**
+ * Used by entities primarily for restricting movement between themselves and also for easily implementing
+ * event based logic when two such entities collide. Whether or not a collision box can be passed through
+ * or not is defferred to the collision box itself as opposed to the implementer of the interface for greater
+ * flexibility allowing for intermingling of movemente and event based collision boxes within the same entity.
+ */
 public interface Collidable extends Positionable {
-    public void onCollision();
+    public void onCollision(Collidable entity);
     public boolean isColliding();
     public void addCollisionBox(Point min, Point max);
     public Collection<CollisionBox> getCollisionBoxes();
--- src/main/java/game/entities/CollisionBox.java
+++ src/main/java/game/entities/CollisionBox.java
@@ -5,32 +5,49 @@ import util.parse.obj.*;
 import java.util.Map;
 import java.awt.Point;
 
+/**
+ * For entities implementing collidable. Relatively situated within an entity for easy saving and loading and to reduce
+ * any possible errors that come with having to constantly update any absolute positions atomically with that of the player.
+ * Collision boxes are not thread safe when both entities are dynamic and hence two such entities should not be split off
+ * into separate threads unless further synchronization measures are taken.
+ */
 public class CollisionBox {
     private Point min, max;
-    private Entity entity;
+    private Collidable entity;
+    private boolean passable;
 
     private CollisionBox() {}
 
-    public CollisionBox(Entity entity, ParserBlock block) {
+    public CollisionBox(Collidable entity, ParserBlock block) {
         Map<String, ParserObject> props = block.getProperties();
         this.min = new Point(((ParserInt) props.get("xMin")).getNumber(), ((ParserInt) props.get("xMax")).getNumber());
         this.max = new Point(((ParserInt) props.get("yMin")).getNumber(), ((ParserInt) props.get("yMax")).getNumber());
+        this.passable = ((ParserInt) props.get("passable")).getNumber() == 1; 
         this.entity = entity;
     }
 
-    public CollisionBox(Entity entity, Point min, Point max) {
+    public CollisionBox(Collidable entity, Point min, Point max, boolean passable) {
         this.min = new Point(min);
         this.max = new Point(max);
         this.entity = entity;
+        this.passable = passable;
     }
 
+    /**
+     * Informs both collidable entities when collision occurs although will return false if passable is set to true.
+     */
     public boolean collidesWith(CollisionBox collisionBox) {
-        return !(
+        boolean collides = !(
             this.getRealMax().getX() < collisionBox.getRealMin().getX() ||
             this.getRealMin().getX() > collisionBox.getRealMax().getX() ||
             this.getRealMax().getY() < collisionBox.getRealMin().getY() ||
             this.getRealMin().getY() > collisionBox.getRealMax().getY() 
         );
+        if ( collides ) {
+            entity.onCollision(collisionBox.getCollidableEntity());
+            collisionBox.getCollidableEntity().onCollision(entity);
+        }
+        return collides && !passable;
     }
 
     public Point getRealMin() { 
@@ -48,10 +65,13 @@ public class CollisionBox {
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
         sb.append("xMin: " + min.getX());
-        sb.append(",xMax: " + max.getX());
-        sb.append(",yMin: " + min.getY());
-        sb.append(",yMax: " + max.getY());
+        sb.append(", xMax: " + max.getX());
+        sb.append(", yMin: " + min.getY());
+        sb.append(", yMax: " + max.getY());
+        sb.append(", passable: " + ((passable) ? 1 : 0));
         sb.append("}");
         return sb.toString();
     };
+
+    public Collidable getCollidableEntity() { return entity; }
 }
